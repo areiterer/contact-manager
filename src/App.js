ebac6ad6846f262558bb758093c5579b08d2e9bf
Andreas Reiterer <reiterer.a@gmail.com>
2017-12-09 14:26:56 +0100

    Show ContactList in App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import ContactList from "./components/ContactList";
+
+const dummyContacts = [
+  {
+    name: "Max Mustermann"
+  },
+  {
+    name: "Jane Doe"
+  },
+  {
+    name: "John Juarez"
+  },
+  {
+    name: "Barry Moon"
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContactList contacts={dummyContacts} />
       </div>
     );
   }
