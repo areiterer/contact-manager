13978813dfddfa92e4e4d2ea3547acbee9833d54
Andreas Reiterer <reiterer.a@gmail.com>
2017-12-12 19:59:41 +0100

    Connect Contacts component + Master Detail View

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,6 +1,41 @@
-import React from "react";
-import SearchableContactList from "../components/SearchableContactList";
+import React, { Component } from "react";
 
-const Contacts = props => <SearchableContactList />;
+import ContactList from "../components/ContactList";
+import ContactDetail from "../components/ContactDetail";
+
+class Contacts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [],
+      selectedContact: null
+    };
+
+    this.handleContactSelected = this.handleContactSelected.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8000/contacts")
+      .then(result => result.json())
+      .then(data => this.setState({ contacts: data }));
+  }
+
+  handleContactSelected(contactId) {
+    this.setState({ selectedContact: contactId });
+  }
+
+  render() {
+    const selectedContactId = this.props.match.params.id;
+    const selectedContact = this.state.contacts.find(
+      c => c.id === selectedContactId
+    );
+    return (
+      <div>
+        <ContactList contacts={this.state.contacts} />
+        {selectedContact && <ContactDetail contact={selectedContact} />}
+      </div>
+    );
+  }
+}
 
 export default Contacts;
