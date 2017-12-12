1f2334a762435efb4c54fd834e4a0149f4f4f083
Andreas Reiterer <reiterer.a@gmail.com>
2017-12-12 20:07:43 +0100

    added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Header = props => <div>Header</div>;
+import "./Header.css";
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contacts">Contacts</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Header;
