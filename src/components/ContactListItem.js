cd8cb0cdb29b9ae163aa1f5c89712c4bff95edf3
Andreas Reiterer <reiterer.a@gmail.com>
2017-12-12 19:59:11 +0100

    Link for DetailView

--- src/components/ContactListItem.js
+++ src/components/ContactListItem.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./ContactListItem.css";
 
 const ContactListItem = props => {
   const contact = props.contact;
   return (
     <div className="contact-list-item">
-      {`${contact.firstName} ${contact.lastName}`}
+      <Link to={`/contacts/${contact.id}`}>
+        {`${contact.firstName} ${contact.lastName}`}
+      </Link>
     </div>
   );
 };
