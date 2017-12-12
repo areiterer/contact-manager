3c5b217db9589521eab99c6b0ea8be164900e3d7
Andreas Reiterer <reiterer.a@gmail.com>
2017-12-12 18:12:45 +0100

    refactor App: Main+Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import SearchableContactList from "./container/SearchableContactList";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SearchableContactList />
+        <Header />
+        <Main />
       </div>
     );
   }
