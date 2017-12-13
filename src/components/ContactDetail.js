d5d43ef2e887ecbf1394ae74bf3c2bc021c1e5f8
Andreas Reiterer <reiterer.a@gmail.com>
2017-12-13 21:29:25 +0100

    show contact detail data

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -2,8 +2,25 @@ import React from "react";
 
 import "./ContactDetail.css";
 
-const ContactDetail = props => (
-  <div className="contactDetail">{JSON.stringify(props.contact)}</div>
-);
+const ContactDetail = props => {
+  const contact = props.contact;
+
+  return (
+    <div className="contactDetail">
+      <p>
+        <span className="label">First name: </span>
+        <span className="value">{contact.firstName}</span>
+      </p>
+      <p>
+        <span className="label">Last name: </span>
+        <span className="value">{contact.lastName}</span>
+      </p>
+      <p>
+        <span className="label">Email: </span>
+        <span className="value">{contact.email}</span>
+      </p>
+    </div>
+  );
+};
 
 export default ContactDetail;
