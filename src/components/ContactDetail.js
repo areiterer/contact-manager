f8c980766ab598d771687325ecd59667e619b053
Andreas Reiterer <reiterer.a@gmail.com>
2017-12-18 14:01:19 +0100

    ContactDetail: Add space for "Notes"

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -7,20 +7,25 @@ const ContactDetail = props => {
 
   return (
     <div className="contactDetail">
-      <h1>
-        {contact.firstName} {contact.lastName}
-      </h1>
-      <div id="address">
-        <p>
-          {contact.street} {contact.house} <br />
-          {contact.zip} {contact.city}
-        </p>
+      <div id="info">
+        <h1>
+          {contact.firstName} {contact.lastName}
+        </h1>
+        <div id="address">
+          <p>
+            {contact.street} {contact.house} <br />
+            {contact.zip} {contact.city}
+          </p>
+        </div>
+        <div className="contactInfo">
+          <p>{contact.phone}</p>
+        </div>
+        <div className="contactInfo">
+          <p>{contact.email}</p>
+        </div>
       </div>
-      <div className="contactInfo">
-        <p>{contact.phone}</p>
-      </div>
-      <div className="contactInfo">
-        <p>{contact.email}</p>
+      <div id="notes">
+        <h2>Notes</h2>
       </div>
     </div>
   );
