1f01b52a42dc5d296e5ed66eade4b0261babf41a
Andreas Reiterer <reiterer.a@gmail.com>
2017-12-20 08:31:00 +0100

    refactor ContactDetail to SFC

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -1,53 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import "./ContactDetail.css";
 
 import ContactEditForm from "./ContactEditForm";
 import ContactDetailView from "./ContactDetailView";
 
-class ContactDetail extends Component {
-  constructor(props) {
-    super(props);
+const ContactDetail = props => {
+  const selectedContact = props.contact;
 
-    this.state = {
-      isEditing: false
-    };
+  if (selectedContact) {
+    return (
+      <div id="contactDetail">
+        {props.editing ? (
+          <ContactEditForm
+            contact={selectedContact}
+            onCancel={props.onCancelEdit}
+            onSave={props.onSaveContact}
+          />
+        ) : (
+          <ContactDetailView
+            onEdit={props.onEditContact}
+            contact={selectedContact}
+          />
+        )}
+      </div>
+    );
+  } else {
+    return (
+      <div className="contactDetail">
+        <p>Please select a Contact</p>
+      </div>
+    );
   }
-
-  componentWillReceiveProps(nextProps) {
-    // If another contact is selected from the list and we're in edit mode, cancel the edit mode
-    if (this.props.match.params.id !== nextProps.match.params.id) {
-      this.setState({ isEditing: false });
-    }
-  }
-
-  render() {
-    const selectedContact = this.props.contact;
-
-    if (selectedContact) {
-      return (
-        <div id="contactDetail">
-          {this.props.editing ? (
-            <ContactEditForm
-              contact={selectedContact}
-              onCancel={this.props.onCancelEdit}
-              onSave={this.props.onSaveContact}
-            />
-          ) : (
-            <ContactDetailView
-              onEdit={this.props.onEditContact}
-              contact={selectedContact}
-            />
-          )}
-        </div>
-      );
-    } else {
-      return (
-        <div className="contactDetail">
-          <p>Please select a Contact</p>
-        </div>
-      );
-    }
-  }
-}
+};
 
 export default ContactDetail;
